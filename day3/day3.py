3b296d5cd56da9ae0b1c01c26632c25f97beebef
Carlos Baumont <baumont.carlos@gmail.com>
2024-12-04 21:43:10 +0000

    Day 3 part 2

--- day3/day3.py
+++ day3/day3.py
@@ -4,8 +4,13 @@ from day1.day1 import read_file_as_list
 
 
 def find_multiply_operations(corrupted: str):
-    multiply_functions = re.findall(r"mul\(\d\d?\d?,\d\d?\d?\)", corrupted)
-    return multiply_functions
+    multiply_operations = re.findall(r"mul\(\d\d?\d?,\d\d?\d?\)", corrupted)
+    return multiply_operations
+
+
+def find_all_operations(corrupted: str):
+    all_operations = re.findall(r"mul\(\d\d?\d?,\d\d?\d?\)|do\(\)|don't\(\)", corrupted)
+    return all_operations
 
 
 def multiply(operation: str) -> int:
@@ -15,11 +20,28 @@ def multiply(operation: str) -> int:
     return number_a * number_b
 
 
+def multiply_with_commands(operations_list: list):
+    acc = 0
+    command = "do()"
+    for operation in operations_list:
+        if operation == "don't()" or operation == "do()":
+            command = operation
+            continue
+        if command != "don't()":
+            acc += multiply(operation)
+
+    return acc
+
+
 def multiplications_sum():
     corrupted_list = read_file_as_list("inputs/day3_input.txt")
 
     operations_lists = [find_multiply_operations(corrupted) for corrupted in corrupted_list]
 
-    result = sum(multiply(operation) for operation_list in operations_lists for operation in operation_list)
+    result_1 = sum(multiply(operation) for operation_list in operations_lists for operation in operation_list)
 
-    return result
+    all_operations_list = [find_all_operations(corrupted) for corrupted in corrupted_list]
+
+    result_2 = sum(multiply_with_commands(all_operations) for all_operations in all_operations_list)
+
+    return result_2
