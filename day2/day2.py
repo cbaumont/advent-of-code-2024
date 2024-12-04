a3b491ae53bc2d10fd9c8bccf08c9dbc0e44e34d
Carlos Baumont <baumont.carlos@gmail.com>
2024-12-04 10:11:43 +0000

    Day 2 part 2

--- day2/day2.py
+++ day2/day2.py
@@ -1,23 +1,34 @@
 from day1.day1 import read_file_as_list
 
 
-def is_report_safe(report: list[int]):
-    is_safe = False
-    is_increasing = True
-    is_decreasing = True
+def remove_item(original_list: list[int], index: int) -> list[int]:
+    modified_list = original_list[:]
+    modified_list.pop(index)
 
-    for i in range(len(report) - 1):
-        dif = report[i] - report[i + 1]
-        if dif in range(1, 4) and is_decreasing:
-            is_safe = True
-            is_increasing = False
-        elif dif in range(-1, -4, -1) and is_increasing:
-            is_safe = True
-            is_decreasing = False
-        else:
-            return False
+    return modified_list
 
-    return is_safe
+
+def is_report_safe(report: list[int], is_increasing: bool = True, is_decreasing: bool = True):
+    if len(report) <= 1: return True
+
+    dif = report[0] - report[1]
+    if dif in range(1, 4) and is_decreasing:
+        return is_report_safe(remove_item(report, 0), False, True)
+    if dif in range(-1, -4, -1) and is_increasing:
+        return is_report_safe(remove_item(report, 0), True, False)
+
+    return False
+
+
+def is_report_safe_problem_dampener(report: list[int]):
+    if is_report_safe(report):
+        return True
+
+    for i in range(len(report)):
+        if is_report_safe(remove_item(report, i)):
+            return True
+
+    return False
 
 
 def count_safe_reports():
@@ -25,6 +36,6 @@ def count_safe_reports():
 
     reports = [[int(level) for level in raw_report.split(" ")] for raw_report in raw_reports]
 
-    count = sum(1 for report in reports if is_report_safe(report))
+    count = sum(1 for report in reports if is_report_safe_problem_dampener(report))
 
     return count
